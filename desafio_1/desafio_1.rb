1d4e08a5803adcc109ecc3a38bcf1b8cb7d44e4c
Cleiton Corrêa <cleitoncorreas@gmail.com>
2024-05-02 22:03:00 -0300

    Primeiro desafio

---
+++ desafio_1/desafio_1.rb
@@ -0,0 +1,14 @@
+# Solicita ao usuário seu nome
+print "Digite seu nome: "
+nome = gets.chomp
+
+# Solicita ao usuário seu sobrenome
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+# Solicita ao usuário sua idade
+print "Digite sua idade: "
+idade = gets.chomp
+
+# Imprime todas as informações em uma única frase
+puts "Olá, #{nome} #{sobrenome}! Você tem #{idade} anos."
